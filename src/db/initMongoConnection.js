ab84f85ede87244123be8d63010904ccda0a6cb8
Denis Bochev <denisbo4ev@gmail.com>
2025-04-05 13:39:59 +0300

    Initial commit

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import { env } from '../utils/env.js';
+
+export async function initMongoConnection() {
+  try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+
+    await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}`);
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
+  }
+}
